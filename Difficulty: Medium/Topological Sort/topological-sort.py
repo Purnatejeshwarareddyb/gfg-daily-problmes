61fc0f408fabcaf3e72b0ad11eccef1efddd9452
Purnatejeshwarareddyb <146438399+Purnatejeshwarareddyb@users.noreply.github.com>
2026-07-21 11:26:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological Sort/topological-sort.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topoSort(self, V, edges):
+        # Code hear
+        adj = [[] for _ in range(V)]
+        for u, v in edges:
+            adj[u].append(v)
+            
+        visited = [False] * V
+        stack = []
+        
+        def dfs(node):
+            visited[node] = True
+            for neighbor in adj[node]:
+                if not visited[neighbor]:
+                    dfs(neighbor)
+            stack.append(node)
+            
+        for i in range(V):
+            if not visited[i]:
+                dfs(i)
+                
+        return stack[::-1]
