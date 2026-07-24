f0aa4ee17ea0edba44206b3ad2f0e9226ce157e6
Purnatejeshwarareddyb <146438399+Purnatejeshwarareddyb@users.noreply.github.com>
2026-07-24 11:55:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Consecutive Path in Binary tree/longest-consecutive-path-in-binary-tree.py
@@ -0,0 +1,36 @@
+'''
+Structure of Binary Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+class Solution:
+    def longestConsecutive(self, root):
+        # code hear
+        if not root:
+            return -1
+        
+        self.max_len = 0
+        
+        def dfs(node, parent_val, current_len):
+            if not node:
+                return
+            
+            if parent_val is not None and node.data == parent_val + 1:
+                current_len += 1
+            else:
+                current_len = 1
+                
+            self.max_len = max(self.max_len, current_len)
+            
+            dfs(node.left, node.data, current_len)
+            dfs(node.right, node.data, current_len)
+            
+        dfs(root, None, 1)
+        
+        return self.max_len if self.max_len > 1 else -1
+
+        
