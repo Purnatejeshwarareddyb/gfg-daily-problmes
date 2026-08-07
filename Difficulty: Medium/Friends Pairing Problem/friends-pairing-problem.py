f2dcdd33912a52f224f56aea344d7a7422d9bc2b
Purnatejeshwarareddyb <146438399+Purnatejeshwarareddyb@users.noreply.github.com>
2026-08-07 12:33:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countFriendsPairings(self, n: int) -> int:
+        # code hear
+        
+        if n <= 2:
+            return n
+        
+        a, b = 1, 2
+        for i in range(3, n + 1):
+            c = b + (i - 1) * a
+            a, b = b, c
+            
+        return b
